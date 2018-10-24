e59c13eb59ea6b9ec3a82593dc04f033cb988faf
Stephen Lake <stephen@closurecode.com>
2018-10-24 12:45:46 +0200

    Fix tests

--- src/Helpers/Shovel.php
+++ src/Helpers/Shovel.php
@@ -2,12 +2,8 @@
 
 function shovel($data = null, $status_code = 200)
 {
-    if ($data) {
-        $shovel = app('shovel');
-        $shovel->provideData($data, $status_code);
+    $shovel = app('shovel');
+    $shovel->provideData($data, $status_code);
 
-        return $shovel->responseInstance();
-    }
-
-    return app('shovel');
+    return $shovel->responseInstance();
 }
