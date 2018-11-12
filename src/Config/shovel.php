5a3763fffb6ea47b85487007bef057908e1bb5dc
Stephen Lake <stephen@closurecode.com>
2018-11-13 01:24:46 +0200

    Finalise naming in config

--- src/Config/shovel.php
+++ src/Config/shovel.php
@@ -7,8 +7,10 @@ return [
     | Pagination
     |--------------------------------------------------------------------------
     |
-    | @TODO
+    | Enabling this option will add the pagination 'links' field  to the
+    | pagination meta data block which contain the URL's to fetch additional
+    | paginated data.
     |
     */
-    'includePagination' => false,
+    'includePaginationLinks' => false,
 ];
