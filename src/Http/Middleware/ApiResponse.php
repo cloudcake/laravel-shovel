d8c78130f35cb2ce321c27cd1c23fd43609e9be1
Stephen Lake <stephen@closurecode.com>
2019-07-15 11:57:42 +0200

    Reduce conditionals

--- src/Http/Middleware/ApiResponse.php
+++ src/Http/Middleware/ApiResponse.php
@@ -14,7 +14,7 @@ use Illuminate\Routing\ResponseFactory;
 
 class ApiResponse
 {
-    public function handle($request, Closure $next, ...$options)
+    public function handle($request, Closure $next)
     {
         $response = $next($request);
         $response = When::isTrue($this->shouldBeBuilt($response), function () use ($response, $options) {
@@ -55,58 +55,56 @@ class ApiResponse
     {
         $range = substr($code, 0, 1);
 
-        switch ($range) {
-           case 4:
-           case 5:
-              return 'error';
-           default:
-              return 'success';
+        if (in_array($range, [4, 5])) {
+            return 'error';
         }
+
+        return 'success';
     }
 
     private function getStatusMessage($code)
     {
-        switch ($code) {
-          case 100: return 'Continue';
-          case 101: return 'Switching Protocols';
-          case 200: return 'OK';
-          case 201: return 'Created';
-          case 202: return 'Accepted';
-          case 203: return 'Non-Authoritative Information';
-          case 204: return 'No Content';
-          case 205: return 'Reset Content';
-          case 206: return 'Partial Content';
-          case 300: return 'Multiple Choices';
-          case 301: return 'Moved Permanently';
-          case 302: return 'Moved Temporarily';
-          case 303: return 'See Other';
-          case 304: return 'Not Modified';
-          case 305: return 'Use Proxy';
-          case 400: return 'Bad Request';
-          case 401: return 'Unauthorized';
-          case 402: return 'Payment Required';
-          case 403: return 'Forbidden';
-          case 404: return 'Not Found';
-          case 405: return 'Method Not Allowed';
-          case 406: return 'Not Acceptable';
-          case 407: return 'Proxy Authentication Required';
-          case 408: return 'Request Time-out';
-          case 409: return 'Conflict';
-          case 410: return 'Gone';
-          case 411: return 'Length Required';
-          case 412: return 'Precondition Failed';
-          case 413: return 'Request Entity Too Large';
-          case 414: return 'Request-URI Too Large';
-          case 415: return 'Unsupported Media Type';
-          case 500: return 'Internal Server Error';
-          case 501: return 'Not Implemented';
-          case 502: return 'Bad Gateway';
-          case 503: return 'Service Unavailable';
-          case 504: return 'Gateway Time-out';
-          case 505: return 'HTTP Version not supported';
+        $codes = [
+            '100' => 'Continue',
+            '101' => 'Switching Protocols',
+            '200' => 'OK',
+            '201' => 'Created',
+            '202' => 'Accepted',
+            '203' => 'Non-Authoritative Information',
+            '204' => 'No Content',
+            '205' => 'Reset Content',
+            '206' => 'Partial Content',
+            '300' => 'Multiple Choices',
+            '301' => 'Moved Permanently',
+            '302' => 'Moved Temporarily',
+            '303' => 'See Other',
+            '304' => 'Not Modified',
+            '305' => 'Use Proxy',
+            '400' => 'Bad Request',
+            '401' => 'Unauthorized',
+            '402' => 'Payment Required',
+            '403' => 'Forbidden',
+            '404' => 'Not Found',
+            '405' => 'Method Not Allowed',
+            '406' => 'Not Acceptable',
+            '407' => 'Proxy Authentication Required',
+            '408' => 'Request Time-out',
+            '409' => 'Conflict',
+            '410' => 'Gone',
+            '411' => 'Length Required',
+            '412' => 'Precondition Failed',
+            '413' => 'Request Entity Too Large',
+            '414' => 'Request-URI Too Large',
+            '415' => 'Unsupported Media Type',
+            '500' => 'Internal Server Error',
+            '501' => 'Not Implemented',
+            '502' => 'Bad Gateway',
+            '503' => 'Service Unavailable',
+            '504' => 'Gateway Time-out',
+            '505' => 'HTTP Version not supported',
+        ];
 
-          default: return 'unknown';
-      }
+        return $codes[$code] ?? 'Unknown';
     }
 
     private function shouldBeBuilt($response)
