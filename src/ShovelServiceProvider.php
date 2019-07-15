726beb08f65a6f5e9266bac40b32c8a7c9251a3b
Stephen Lake <stephen@closurecode.com>
2019-07-16 00:49:11 +0200

    Replace individual registration with facade

--- src/ShovelServiceProvider.php
+++ src/ShovelServiceProvider.php
@@ -3,10 +3,8 @@
 namespace Shovel;
 
 use Illuminate\Support\Arr;
-use Illuminate\Http\Response;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
 
 class ShovelServiceProvider extends ServiceProvider
 {
@@ -31,13 +29,9 @@ class ShovelServiceProvider extends ServiceProvider
      */
     private function registerMacros()
     {
-        $withMeta = function ($key, $value) {
+        Response::macro('withMeta', function ($key, $value) {
             Arr::set($this->additionalMeta, $key, $value);
             return $this;
-        };
-
-        Response::macro('withMeta', $withMeta);
-        JsonResponse::macro('withMeta', $withMeta);
-        ResponseFactory::macro('withMeta', $withMeta);
+        });
     }
 }
