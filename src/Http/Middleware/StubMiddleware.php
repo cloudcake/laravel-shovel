ae5a2401c94abc895d62876592d1dfb37daa15df
Stephen Lake <stephen@closurecode.com>
2019-07-15 12:19:41 +0200

    Add phpdoc

--- src/Http/Middleware/StubMiddleware.php
+++ src/Http/Middleware/StubMiddleware.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Response;
 use Shovel\Http\Middleware\ApiResponse as Middleware;
 
 class ApiResponse extends Middleware
 {
-    protected function beforeResponding($response)
+    /**
+     * Allow transforming of response before it is returned.
+     *
+     * @param Illuminate\Http\Response $response
+     * @return Illuminate\Http\Response
+     */
+    protected function beforeResponding(Response $response)
     {
-       //
+        //
     }
 }
