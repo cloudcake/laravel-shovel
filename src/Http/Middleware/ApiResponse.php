2dff07862ecf18404166491f99c0748c4e7334b1
Stephen Lake <stephen@closurecode.com>
2019-07-15 12:03:39 +0200

    Move code constants to interface

--- src/Http/Middleware/ApiResponse.php
+++ src/Http/Middleware/ApiResponse.php
@@ -3,6 +3,7 @@
 namespace Shovel\Http\Middleware;
 
 use Closure;
+use Shovel\HTTP;
 use ArrayObject;
 use Commons\When;
 use JsonSerializable;
@@ -12,7 +13,7 @@ use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Routing\ResponseFactory;
 
-class ApiResponse
+class ApiResponse implements HTTP
 {
     public function handle($request, Closure $next)
     {
@@ -68,47 +69,7 @@ class ApiResponse
 
     private function getStatusMessage($code)
     {
-        $codes = [
-            '100' => 'Continue',
-            '101' => 'Switching Protocols',
-            '200' => 'OK',
-            '201' => 'Created',
-            '202' => 'Accepted',
-            '203' => 'Non-Authoritative Information',
-            '204' => 'No Content',
-            '205' => 'Reset Content',
-            '206' => 'Partial Content',
-            '300' => 'Multiple Choices',
-            '301' => 'Moved Permanently',
-            '302' => 'Moved Temporarily',
-            '303' => 'See Other',
-            '304' => 'Not Modified',
-            '305' => 'Use Proxy',
-            '400' => 'Bad Request',
-            '401' => 'Unauthorized',
-            '402' => 'Payment Required',
-            '403' => 'Forbidden',
-            '404' => 'Not Found',
-            '405' => 'Method Not Allowed',
-            '406' => 'Not Acceptable',
-            '407' => 'Proxy Authentication Required',
-            '408' => 'Request Time-out',
-            '409' => 'Conflict',
-            '410' => 'Gone',
-            '411' => 'Length Required',
-            '412' => 'Precondition Failed',
-            '413' => 'Request Entity Too Large',
-            '414' => 'Request-URI Too Large',
-            '415' => 'Unsupported Media Type',
-            '500' => 'Internal Server Error',
-            '501' => 'Not Implemented',
-            '502' => 'Bad Gateway',
-            '503' => 'Service Unavailable',
-            '504' => 'Gateway Time-out',
-            '505' => 'HTTP Version not supported',
-        ];
-
-        return $codes[$code] ?? 'Unknown';
+        return self::CODES[$code] ?? 'Unknown';
     }
 
     private function shouldBeBuilt($response)
