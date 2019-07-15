4482ef778069f872a5033fa381603c1e6fbd33a9
Stephen Lake <stephen@closurecode.com>
2019-07-15 12:31:52 +0200

    Reinstate phpdoc

--- src/Http/Middleware/ApiResponse.php
+++ src/Http/Middleware/ApiResponse.php
@@ -3,43 +3,69 @@
 namespace Shovel\Http\Middleware;
 
 use Closure;
+use Shovel\HTTP;
 use ArrayObject;
 use Commons\When;
 use JsonSerializable;
 use Illuminate\Http\Response;
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Routing\ResponseFactory;
 
-class ApiResponse
+class ApiResponse implements HTTP
 {
+    /**
+     * Handle the response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure  $next
+     * @param string[] ...$options
+     * @return \Illuminate\Http\Response
+     */
     public function handle($request, Closure $next, ...$options)
     {
         $response = $next($request);
-        $response = When::isTrue($this->shouldBeBuilt($response), function () use ($response) {
+        $response = When::isTrue($this->shouldBeBuilt($response), function () use ($response, $options) {
             $this->beforeResponding($response);
-            return $this->buildPayload($response);
+            return $this->buildPayload($response, ...$options);
         }, $response);
 
         return $response;
     }
 
+    /**
+     * Allow transforming of response before it is returned.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return \Illuminate\Http\Response
+     */
     protected function beforeResponding($response)
     {
         return $response;
     }
 
-    private function buildPayload($response, $metaTag = 'meta', $dataTag = 'data', $paginationTag = 'pagination')
+    /**
+     * Construct the response payload.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @param string[] ...$options
+     * @return \Illuminate\Http\Response
+     */
+    private function buildPayload($response, ...$options)
     {
+        $metaTag = $options[0] ?? 'meta';
+        $dataTag = $options[1] ?? 'data';
+        $pageTag = $options[2] ?? 'pagination';
+
         $payload = $this->getMetaBlock($response, $metaTag);
 
         if ($response->content()) {
             if ($this->isPaginated($response)) {
-                $payload[$metaTag][$paginationTag] = $this->getPaginationBlock($response->original);
+                $payload[$metaTag][$pageTag] = $this->getPaginationBlock($response->original);
                 $payload[$dataTag] = $response->original->items();
             } elseif ($this->isPaginatedCollection($response)) {
-                $payload[$metaTag][$paginationTag] = $this->getPaginationBlock($response->original->resource);
+                $payload[$metaTag][$pageTag] = $this->getPaginationBlock($response->original->resource);
                 $payload[$dataTag] = $response->original->resource->items();
             } else {
                 $payload[$dataTag] = json_decode($response->content());
@@ -51,64 +77,40 @@ class ApiResponse
         return $response;
     }
 
-    private function getStatus($code)
+    /**
+     * Returns a string defining whether or not the response is successful.
+     *
+     * @param int $code
+     * @return string
+     */
+    private function getStatus(int $code)
     {
         $range = substr($code, 0, 1);
 
-        switch ($range) {
-           case 4:
-           case 5:
-              return 'error';
-           default:
-              return 'success';
+        if (in_array($range, [4, 5])) {
+            return 'error';
         }
+
+        return 'success';
     }
 
-    private function getStatusMessage($code)
+    /**
+     * Returns the text representation of the HTTP status code.
+     *
+     * @param int $code
+     * @return string
+     */
+    private function getStatusMessage(int $code)
     {
-        switch ($code) {
-          case 100: return 'Continue';
-          case 101: return 'Switching Protocols';
-          case 200: return 'OK';
-          case 201: return 'Created';
-          case 202: return 'Accepted';
-          case 203: return 'Non-Authoritative Information';
-          case 204: return 'No Content';
-          case 205: return 'Reset Content';
-          case 206: return 'Partial Content';
-          case 300: return 'Multiple Choices';
-          case 301: return 'Moved Permanently';
-          case 302: return 'Moved Temporarily';
-          case 303: return 'See Other';
-          case 304: return 'Not Modified';
-          case 305: return 'Use Proxy';
-          case 400: return 'Bad Request';
-          case 401: return 'Unauthorized';
-          case 402: return 'Payment Required';
-          case 403: return 'Forbidden';
-          case 404: return 'Not Found';
-          case 405: return 'Method Not Allowed';
-          case 406: return 'Not Acceptable';
-          case 407: return 'Proxy Authentication Required';
-          case 408: return 'Request Time-out';
-          case 409: return 'Conflict';
-          case 410: return 'Gone';
-          case 411: return 'Length Required';
-          case 412: return 'Precondition Failed';
-          case 413: return 'Request Entity Too Large';
-          case 414: return 'Request-URI Too Large';
-          case 415: return 'Unsupported Media Type';
-          case 500: return 'Internal Server Error';
-          case 501: return 'Not Implemented';
-          case 502: return 'Bad Gateway';
-          case 503: return 'Service Unavailable';
-          case 504: return 'Gateway Time-out';
-          case 505: return 'HTTP Version not supported';
-
-          default: return 'unknown';
-      }
+        return self::CODES[$code] ?? 'Unknown';
     }
 
+    /**
+     * Determines if the response should be handled by Shovel.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return bool
+     */
     private function shouldBeBuilt($response)
     {
         return ($response->status() != 500) && (
@@ -121,17 +123,36 @@ class ApiResponse
         );
     }
 
+    /**
+     * Returns true if the response is a paginated object.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return bool
+     */
     private function isPaginated($response)
     {
         return $response->original instanceof LengthAwarePaginator;
     }
 
+    /**
+     * Returns true if the response is a paginated collection.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return bool
+     */
     private function isPaginatedCollection($response)
     {
         return isset($response->original->resource) &&
                $response->original->resource instanceof LengthAwarePaginator;
     }
 
+    /**
+     * Constructs and returns the meta object.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @param string $metaTag
+     * @return array
+     */
     private function getMetaBlock($response, $metaTag)
     {
         $payload = [
@@ -149,6 +170,12 @@ class ApiResponse
         return $payload;
     }
 
+    /**
+     * Constructs and returns the pagination object.
+     *
+     * @param \Illuminate\Http\Response $response
+     * @return array
+     */
     private function getPaginationBlock($paginator)
     {
         return [
