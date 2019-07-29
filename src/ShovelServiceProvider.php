fa5e2227ec0ce82b030e5bf24e19da6377fc0c9e
Stephen Lake <stephen@closurecode.com>
2019-07-30 01:23:16 +0200

    No need to publish middleware

--- src/ShovelServiceProvider.php
+++ src/ShovelServiceProvider.php
@@ -11,26 +11,29 @@ use Illuminate\Support\ServiceProvider;
 class ShovelServiceProvider extends ServiceProvider
 {
     /**
+     * The middleware that should handle HTTP requests.
+     *
+     * @var string
+     */
+    protected $requestMiddleware = \Shovel\Http\Middleware\ApiRequest::class;
+
+    /**
+     * The middleware that should handle HTTP responses.
+     *
+     * @var string
+     */
+    protected $responseMiddleware = \Shovel\Http\Middleware\ApiResponse::class;
+
+    /**
      * Boot up Shovel.
      *
      * @return void
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/Http/Middleware/StubMiddleware.php' => app_path('Http/Middleware/ApiResponse.php'),
-        ], 'middleware');
+        $this->app['router']->aliasMiddleware('ApiRequest', $this->requestMiddleware);
+        $this->app['router']->aliasMiddleware('ApiResponse', $this->responseMiddleware);
 
-        $this->registerMacros();
-    }
-
-    /**
-     * Register Shovel macros.
-     *
-     * @return void
-     */
-    private function registerMacros()
-    {
         $withMeta = function ($key, $value) {
             Arr::set($this->additionalMeta, $key, $value);
             return $this;
